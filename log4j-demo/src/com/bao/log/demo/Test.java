08e9b251c7b4b5a8418c423212a8388fac72ee2f
sunbao <sunbao@corp.netease.com>
2017-10-24 09:26:08 +0800

    日志功能demo

---
+++ log4j-demo/src/com/bao/log/demo/Test.java
@@ -0,0 +1,24 @@
+package com.bao.log.demo;
+
+import org.apache.commons.logging.Log; 
+import org.apache.commons.logging.LogFactory; 
+
+public class Test { 
+
+   // 在任何需要记录日志的类中 
+   Log log = LogFactory.getLog( this .getClass()); 
+   public void one() { 
+      log .info( "into one method" ); 
+      try { 
+         System. out .println(9/0); 
+      } catch (RuntimeException e) { 
+         log .error(e.getMessage()); 
+      } 
+      log .info( "out one method" ); 
+   } 
+
+   public static void main(String[] args) { 
+      new Test().one(); 
+   } 
+} 
+
