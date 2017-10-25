62baf69785d477cb8b657b7ccb978487c5aca594
sunbao <sunbao@corp.netease.com>
2017-10-25 09:20:34 +0800

    根据日志级别输出日志

--- log4j-demo/src/com/bao/log/demo/Test.java
+++ log4j-demo/src/com/bao/log/demo/Test.java
@@ -8,13 +8,15 @@ public class Test {
    // 在任何需要记录日志的类中 
    Log log = LogFactory.getLog( this .getClass()); 
    public void one() { 
-      log .info( "into one method" ); 
+	  if(log.isInfoEnabled())
+		  log .info( "into one method" ); 
       try { 
          System. out .println(9/0); 
       } catch (RuntimeException e) { 
          log .error(e.getMessage()); 
       } 
-      log .info( "out one method" ); 
+      if(log.isInfoEnabled())
+    	  log .info( "out one method" ); 
    } 
 
    public static void main(String[] args) { 
