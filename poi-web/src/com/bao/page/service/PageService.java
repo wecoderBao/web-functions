b4970b2d3688053ed9890c9c5c039c8526155f01
sunbao <sunbao@corp.netease.com>
2017-12-27 13:56:43 +0800

    数据库表分页查询

--- poi-web/src/com/bao/page/service/PageService.java
+++ poi-web/src/com/bao/page/service/PageService.java
@@ -2,4 +2,15 @@ package com.bao.page.service;
 
 public class PageService {
 
+	/**
+	 * 
+	 * //分页查询语句
+select * from 表名 where limit page , count;
+和
+//获取表中的总数据，确定页数
+select count(*) from 表名;
+	 * 
+	 * 
+	 * 
+	 */
 }
